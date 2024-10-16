6768afa02546b97c4dc88b1e9ebf2f0c3e9e4c66
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-16 22:24:54 +0530

    Create TotalDownloads.jsx

---
+++ src/components/TotalDownloads.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TotalDownloads() {
+  return (
+    <div>
+      <img
+        src="https://visitor-badge.laobi.icu/badge?page_id=vscode-extension-downloader-total-downloads"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default TotalDownloads;
