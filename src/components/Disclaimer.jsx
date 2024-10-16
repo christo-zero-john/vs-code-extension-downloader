1251937ae6b9f2dbc40568cfcee085d078c2190b
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-16 19:41:36 +0530

    Create Disclaimer.jsx

---
+++ src/components/Disclaimer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Disclaimer() {
+  return (
+    <div className="bg-orange m-3 p-4 rounded ">
+      <p className="text-light fw-500 display-5 mx-auto   border-bottom w-fit">
+        Disclaimer
+      </p>
+      <p className="text-light">
+        This is an independent hobby project and is not affiliated with,
+        endorsed by, or in any way officially connected with Microsoft
+        Corporation or any of their subsidiaries or affiliates. The names
+        Microsoft, VS Code, and related trademarks belong to their respective
+        owners. The developer of this application bears no responsibility for
+        the extensions downloaded or used through this app. Users are solely
+        responsible for ensuring their use of this tool and any downloaded
+        extensions complies with all applicable terms of service, laws, and
+        regulations. Use this application at your own risk.{" "}
+        <p className="fw-700 fs-5">
+          In addition you are not allowed to redestribute the extensions
+          downloaded. They must only be used for personal purpose.
+        </p>
+      </p>
+    </div>
+  );
+}
+
+export default Disclaimer;
