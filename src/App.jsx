9854189c6f1b44c3cb77898f9bb831bdb7482ce6
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-16 19:41:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import GetExtensionLink from "./components/GetExtensionLink";
+import Disclaimer from "./components/Disclaimer";
+import TermsAndConditions from "./components/TermsAndConditions";
 
 function App() {
-  return <div>App</div>;
+  const [link, setLink] = useState("");
+  const [downloadLink, setDownloadLink] = useState("");
+
+  return (
+    <div className="en-Oxanium container-fluid">
+      <h1 className="text-dark text-center">
+        Visual Studio Code Extension Downloader
+      </h1>
+
+      <GetExtensionLink
+        link={{
+          var: link,
+          set: setLink,
+        }}
+        downloadLink={{
+          var: downloadLink,
+          set: setDownloadLink,
+        }}
+      />
+      <Disclaimer />
+      
+      <TermsAndConditions />
+    </div>
+  );
 }
 
 export default App;
