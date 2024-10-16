b07f2f30754265217834c9019231c416c8d35bdd
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-16 22:24:52 +0530

    Create ThanksForDownloading.jsx

---
+++ src/components/ThanksForDownloading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TotalDownloads from "./TotalDownloads";
+
+function ThanksForDownloading() {
+  return (
+    <div>
+      <TotalDownloads />
+    </div>
+  );
+}
+
+export default ThanksForDownloading;
