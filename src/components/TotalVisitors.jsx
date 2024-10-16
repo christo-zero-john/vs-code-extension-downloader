8b9864dd071cb7921c65888f18cb22bd1e47dd33
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-16 22:24:55 +0530

    Create TotalVisitors.jsx

---
+++ src/components/TotalVisitors.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TotalVisitors() {
+  return <div>
+    <img src="https://visitor-badge.laobi.icu/badge?page_id=vscode-extension-downloader" alt="" />
+  </div>;
+}
+
+export default TotalVisitors;
