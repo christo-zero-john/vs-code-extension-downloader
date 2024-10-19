c6ee994c1ba5fa20b88c7d5996c011e0203a6592
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-19 09:32:11 +0530

    Create RecentDownloads.jsx

---
+++ src/components/RecentDownloads.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function RecentDownloads() {
+  return (
+    <div>
+      <p className="">Recent Downloads</p>
+      <div id="recent-downloads"></div>
+    </div>
+  );
+}
+
+export default RecentDownloads;
