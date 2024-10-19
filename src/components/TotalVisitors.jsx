438aee69da85fa22aa8966fb475b85c2f1002fcb
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-19 09:32:15 +0530

    Update TotalVisitors.jsx

--- src/components/TotalVisitors.jsx
+++ src/components/TotalVisitors.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 function TotalVisitors() {
-  return <div>
-    <img src="https://visitor-badge.laobi.icu/badge?page_id=vscode-extension-downloader" alt="" />
+  return <div className="col-12 p-3 pb-0">
+    <img className="col-3 col-md-2" src="https://visitor-badge.laobi.icu/badge?page_id=vscode-extension-downloader&left_color=transparent&right_color=orange" alt="Total Visitors" />
+
   </div>;
 }
 
