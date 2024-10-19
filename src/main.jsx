002ee621e6fe152a9147878a78037374f10d655d
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-19 09:32:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./libraries/bootstrap/css/bootstrap.css";
-import "./libraries/bootstrap/js/bootstrap.js";
+import "./libraries/bootstrap/js/bootstrap.bundle";
 import "./styles/defaults.css";
+import "./styles/custom.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
