dcb828dea1fa806a390d48a2a668bf75b5b6faf2
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-19 09:32:03 +0530

    Create DownloadAsBundle.jsx

---
+++ src/components/DownloadAsBundle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DownloadAsBundle() {
+  return (
+    <div className="bg-dark text-light wd-100 hd-100 d-flex flex-row justify-content-center align-items-center display-1">
+      <p>Stay Updated!!</p>
+    </div>
+  );
+}
+
+export default DownloadAsBundle;
